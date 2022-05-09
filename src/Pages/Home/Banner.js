5c54326b88e5a869b9004c409584fd442a273ec8
Rasujon3 <rasujon3@gmail.com>
2022-05-10 01:27:32 +0600

    71-6 Responsive Grid for services section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,10 +4,7 @@ import bg from "../../assets/images/bg.png";
 
 const Banner = () => {
   return (
-    <div
-      class="hero min-h-screen px-12"
-      style={{ backgroundImage: `url(${bg})` }}
-    >
+    <div class="hero min-h-screen" style={{ backgroundImage: `url(${bg})` }}>
       <div class="hero-content flex-col lg:flex-row-reverse">
         <img src={chair} alt="" class="max-w-sm rounded-lg shadow-2xl" />
         <div>
