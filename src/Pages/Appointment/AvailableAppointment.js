85fa4b14d5b2158b36cc8a902768ce0d76dcf7ba
Rasujon3 <rasujon3@gmail.com>
2022-05-11 02:52:32 +0600

    72-5 Display a modla from on booking click with dynamic data

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
 import Service from "./Service";
+import BookingModal from "./BookingModal";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
   useEffect(() => {
     const url = `services.json`;
     fetch(url)
@@ -17,9 +19,14 @@ const AvailableAppointment = ({ date }) => {
       </h4>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {services.map((service) => (
-          <Service key={service._id} service={service} />
+          <Service
+            key={service._id}
+            service={service}
+            setTreatment={setTreatment}
+          />
         ))}
       </div>
+      {treatment && <BookingModal treatment={treatment}></BookingModal>}
     </div>
   );
 };
