c2655ffe46e535d5990db2b8f6fa375a896a8d78
Rasujon3 <rasujon3@gmail.com>
2022-05-11 01:49:04 +0600

    72-4 Booking Options and availabel slots with conditional rendering

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
+import Service from "./Service";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
@@ -14,7 +15,11 @@ const AvailableAppointment = ({ date }) => {
       <h4 className="text-xl text-secondary font-bold text-center">
         Available Appointments on {format(date, "PP")}
       </h4>
-      <div>card</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {services.map((service) => (
+          <Service key={service._id} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Service = ({ service }) => {
+  const { name, slots } = service;
+  return (
+    <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+      <div class="card-body text-center">
+        <h2 class="text-xl font-bold text-secondary text-center">{name}</h2>
+        <p>
+          {slots.length ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-red-500">Try another date.</span>
+          )}
+        </p>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <div class="card-actions justify-center">
+          <button
+            disabled={slots.length === 0}
+            class="btn btn-secondary text-white uppercase"
+          >
+            Book Appointment
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
