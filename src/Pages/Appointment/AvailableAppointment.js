581917cc0f0c7591457b371d171956003c425e9b
Rasujon3 <rasujon3@gmail.com>
2022-05-11 04:13:51 +0600

    72-9 Module Summary & design polish

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -14,7 +14,7 @@ const AvailableAppointment = ({ date }) => {
   }, []);
   return (
     <div>
-      <h4 className="text-xl text-secondary font-bold text-center">
+      <h4 className="text-xl text-secondary font-bold text-center my-12">
         Available Appointments on {format(date, "PP")}
       </h4>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -21,7 +21,7 @@ const Service = ({ service, setTreatment }) => {
             for="booking-modal"
             disabled={slots.length === 0}
             onClick={() => setTreatment(service)}
-            class="btn btn-secondary text-white uppercase"
+            class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
           >
             Book Appointment
           </label>
