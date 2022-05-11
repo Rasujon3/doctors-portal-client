5661882245008b10a2572fae6a45df18ef11c7c6
Rasujon3 <rasujon3@gmail.com>
2022-05-11 21:27:02 +0600

    73-2 Setup login page, Implement Sign in with Google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
+import auth from "./../../firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  if (user) {
+    console.log(user);
+  }
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="flex justify-center h-screen items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-2xl font-bold text-center">Login</h2>
+          <form></form>
+          <div className="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline uppercase"
+          >
+            Continue with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
