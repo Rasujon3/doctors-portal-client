27cc5c154f590e2545b54417b5ef98034ae7b90e
Rasujon3 <rasujon3@gmail.com>
2022-05-14 00:56:18 +0600

    74-3 Limit one booking per user per treatment per day & react query setup

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -34,9 +34,15 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        if (data.success) {
+          toast.success(`Appointment is set, ${formattedDate} at ${slot}`);
+        } else {
+          toast.error(
+            `Already have an appointment on, ${data.booking?.date} at ${data.booking?.slot}`
+          );
+        }
         // to close the modal
         setTreatment(null);
-        toast("Booking successfully");
       });
   };
 
