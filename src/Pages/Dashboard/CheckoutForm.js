8c37dc084b12e44b0a8524f4f94425268b1513c8
Rasujon3 <rasujon3@gmail.com>
2022-05-18 05:12:12 +0600

    77-7 Process card confirmation for Payment and display success Message

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -5,9 +5,11 @@ const CheckoutForm = ({ appointment }) => {
   const stripe = useStripe();
   const elements = useElements();
   const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [transactionId, setTransactionId] = useState("");
   const [clientSecret, setClientSecret] = useState("");
 
-  const { price } = appointment;
+  const { price, patient, patientName } = appointment;
 
   useEffect(() => {
     const url = `http://localhost:5000/create-payment-intent`;
@@ -43,6 +45,27 @@ const CheckoutForm = ({ appointment }) => {
     });
 
     setCardError(error?.message || "");
+    setSuccess("");
+
+    // confirm card payment
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: patientName,
+            email: patient,
+          },
+        },
+      });
+    if (intentError) {
+      setCardError(intentError?.message);
+    } else {
+      setCardError("");
+      setTransactionId(paymentIntent.id);
+      console.log(paymentIntent);
+      setSuccess("Congrats! Your payment is completed.");
+    }
   };
   return (
     <>
@@ -72,6 +95,15 @@ const CheckoutForm = ({ appointment }) => {
         </button>
       </form>
       {cardError && <p className="text-red-500">{cardError}</p>}
+      {success && (
+        <div className="text-green-500">
+          <p>{success}</p>
+          <p>
+            Your transaction Id:{" "}
+            <spna className="text-orange-500 font-bold">{transactionId}</spna>
+          </p>
+        </div>
+      )}
     </>
   );
 };
